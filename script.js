48c3103c04b43a43ef6c36fed02b9720a1532d21
AD0T.dev <adityakarma1706@gmail.com>
2022-10-06 20:16:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Firebase config key by which we will access our firebase database
 const firebaseConfig = {
   apiKey: "AIzaSyCG7l8aGepjaIwpsAxFyMC8rg09wgsGRfg",
   authDomain: "petmed-b39c6.firebaseapp.com",
@@ -18,11 +19,29 @@ const auth = firebase.auth();
 function signUp() {
   let email = document.getElementById("floatingInput");
   let password = document.getElementById("floatingPassword");
+  let if_user_is_vet = document.getElementById("vetCheckBox");
 
-  const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
-  //
-  promise.catch(e => alert(e.message));
-  alert("SignUp Successfully");
+  auth.createUserWithEmailAndPassword(email.value, password.value).catch(function (error) {
+
+    let errorCode = error.code;
+
+    // if (errorCode == "auth/weak-password") {alert("The password is too weak and the minimum size of password is 6 charecters");}
+    // else if (errorCode == "auth/email-already-in-use") {alert("The email is already in use");email.value = "";password.value = "";}
+    // else if (errorCode == "auth/invalid-email") {alert("The email is invalid");email.value = "";password.value = "";}
+    // else if (errorCode == "auth/operation-not-allowed") {alert("The email is not allowed");email.value = "";password.value = "";}
+    // else if (error.Message == "undefined") {alert("Successfully signed up");email.value = "";password.value = ""; if (if_user_is_vet.checked == true){window.location="vetinfo.html"} else {window.location="vets.html";}}
+    // else {alert("Please try again");email.value = "";password.value = "";}
+
+    //Debug functions
+    console.log(error.Message);
+    alert(error.code)
+    email.value = "";
+    password.value = "";
+    // if (if_user_is_vet.checked == true){window.location="vetinfo.html";}// If checkbox is checked then redirect to vetinfo.html
+    // else {window.location="vets.html";}// If checkbox is not checked then redirect to vets.html
+  });
+  // if (if_user_is_vet.checked == true){window.location="vetinfo.html";}// If checkbox is checked then redirect to vetinfo.html
+  // else {window.location="vets.html";}// If checkbox is not checked then redirect to vets.html
 }
 
 
@@ -33,7 +52,7 @@ function signIn() {
   let password = document.getElementById("floatingPassword");
   auth.signInWithEmailAndPassword(email.value, password.value).catch(function (error) {
 
-    var errorCode = error.code;
+    // var errorCode = error.code;
     console.log(error.Message);
     alert("An error occured, please try again");
     email.value = "";
@@ -62,10 +81,10 @@ firebase.auth().onAuthStateChanged((user) => {
   if (user) {
 
     let email = user.email;
-    console.log("outside redired")
+    // console.log("outside redired")
     if (location.href.split("/").slice(-1) == "signin.html" || location.href.split("/").slice(-1) == "signup.html") {
-      console.log(window.location.href)
-      console.log("redirect")
+      // console.log(window.location.href)
+      // console.log("redirect")
       window.location = 'vets.html';
       alert("Signed in with: " + email);
     }
